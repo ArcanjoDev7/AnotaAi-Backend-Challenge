d008bc38b3b49913a0d4cea23b1fd8523b4bafa8
ArcanjoDev7 <miguelarcanjodearaujocosta@gmail.com>
2024-03-11 15:15:00 -0300

    recapitulando

--- src/main/java/com/miguelarcanjo/desafioanotaai/controllers/CategoryController.java
+++ src/main/java/com/miguelarcanjo/desafioanotaai/controllers/CategoryController.java
@@ -21,22 +21,26 @@ public class CategoryController {
     public ResponseEntity<Category> insert(@RequestBody CategoryDTO categoryData){
         Category newCategory = this.service.insert(categoryData);
         return ResponseEntity.ok().body(newCategory);
+
     }
 
     @GetMapping
     public ResponseEntity<List<Category>> getAll(){
             List<Category> categories = this.service.getAll();
             return ResponseEntity.ok().body(categories);
+
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO categoryData){
         Category updateCategory = this.service.update(id, categoryData);
         return ResponseEntity.ok().body(updateCategory);
+
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<Category> delete(@PathVariable("id") String id){
         this.service.delete(id);
         return ResponseEntity.noContent().build();
+
     }
 }
